9faea4b3dabf8b5a3c01fd67c002d52c7f1293fe
Bradley Wood <github@bradleywood.com>
2013-11-08 14:34:36 +0000

    basic tell controls working

--- UniversalRemote/UniversalRemote.ino
+++ UniversalRemote/UniversalRemote.ino
@@ -10,7 +10,7 @@
 
 
 //format {{PROTOCOL},{"devname"},{"buttname"}, data, nbits,len, buf[],hz,addr,repeat}
-ir_transmission_t cmd_off[1] = { 
+ir_transmission_t cmdOnOff[1] = { 
 	{ SONY, TV, 0xA90, 12, 0, {0}, 0, 0, 0 } 
 };
 
@@ -24,7 +24,7 @@ void setup()
 void loop() {
   if (Serial.read() != -1) {
     for (int i = 0; i < 3; i++) {
-      irsend.sendSony(cmd_off[0].data, cmd_off[0].nbits); // Sony TV power code
+      irsend.sendSony(cmdOnOff[0].data, cmdOnOff[0].nbits); // Sony TV power code
       delay(40);
     }
   }
